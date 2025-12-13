b2bd4042a455f8f088171be3179eb81da654f822
Michael Francis <michaelmugendi1707@gmail.com>
2025-12-13 12:41:48 +0300

    Update: add dotenv

--- deploy_favorites.py
+++ deploy_favorites.py
@@ -5,13 +5,16 @@ def main():
     print("Read in the vyper code and deploy it!")
     with open("buy_me_a_coffee.vy", "r") as favorites_file:
         favorites_code = favorites_file.read()
-        compliation_details = compile_code(favorites_code, output_formats=["bytecode"])
-        print("Compilation Details:", compliation_details)
+        compliation_details = compile_code(favorites_code, output_formats=["bytecode", "abi"])
+        # print("Compilation Details:", compliation_details)
 
     
-    w3 = Web3(Web3.HTTPProvider('https://virtual.sepolia.eu.rpc.tenderly.co/69be85b3-80c7-48df-8a41-8053bb3cbb4f'))
-    favorites_contract = w3.eth.contract(bytecode=compliation_details["bytecode"])
-    print(favorites_contract)
+    w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+    favorites_contract = w3.eth.contract(bytecode=compliation_details["bytecode"], abi=compliation_details["abi"])
+    # print(favorites_contract)
+
+    print("Building the transaction to deploy the contract...")
+    transaction = favorites_contract.constructor().build_transaction()
 
 if __name__ == "__main__":
     main()
